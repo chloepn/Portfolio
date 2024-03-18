8df5c627ad8c69bfce0062e43d66aac0963b9d07
chloepn <157665703+chloepn@users.noreply.github.com>
2024-03-17 20:56:02 -0700

    add header and home page

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+export default function Social(){
+    return (
+         <div className="home__social">
+            <a href = "https://github.com/chloepn" className ="home__social-icon" target = "_blank" >
+                <i class="uil uil-github-alt"></i>
+            </a>
+            <a href = "https://www.linkedin.com/in/chloe-phan/" className ="home__social-icon" target = "_blank" >
+                <i class="uil uil-linkedin-alt"></i>
+            </a>
+         </div>
+    );
+}
